45552c78cd18d2e1f2a490d00d965911f3617cad
riku6460 <17585784+riku6460@users.noreply.github.com>
2021-06-21 03:56:54 +0900

    Add support for Spigot 1.17

--- src/main/java/nl/thijsalders/spigotproxy/SpigotProxy.java
+++ src/main/java/nl/thijsalders/spigotproxy/SpigotProxy.java
@@ -5,86 +5,86 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import nl.thijsalders.spigotproxy.netty.NettyChannelInitializer;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.logging.Level;
 
-import nl.thijsalders.spigotproxy.netty.NettyChannelInitializer;
-
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-
 public class SpigotProxy extends JavaPlugin {
-	
-	private String channelFieldName;
-	
-	public void onLoad(){
-		String version = super.getServer().getClass().getPackage().getName().replace(".",  ",").split(",")[3];
-		channelFieldName = getChannelFieldName(version);
-		if(channelFieldName == null){
-			getLogger().log(Level.SEVERE, "Unknown server version " + version + ", please see if there are any updates avaible");
-			return;
-		} else {
-			getLogger().info("Detected server version " + version);
-		}
-		try {
-			getLogger().info("Injecting NettyHandler...");
-			inject();
-			getLogger().info("Injection successful!");
-		} catch (Exception e) {
-			getLogger().info("Injection netty handler failed!");
-			e.printStackTrace();
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void inject() throws Exception {
-		Method serverGetHandle = Bukkit.getServer().getClass().getDeclaredMethod("getServer");
-		Object minecraftServer = serverGetHandle.invoke(Bukkit.getServer());
-		
-		Method serverConnectionMethod = null;
-		for(Method method : minecraftServer.getClass().getSuperclass().getDeclaredMethods()) {
-			if(!method.getReturnType().getSimpleName().equals("ServerConnection")) {
-				continue;
-			}
-			serverConnectionMethod = method;
-			break;
-		}
-		Object serverConnection = serverConnectionMethod.invoke(minecraftServer);
-		List<ChannelFuture> channelFutureList = ReflectionUtils.getPrivateField(serverConnection.getClass(), serverConnection, List.class, channelFieldName);
-		
-		for(ChannelFuture channelFuture : channelFutureList) {
-			ChannelPipeline channelPipeline = channelFuture.channel().pipeline();
-			ChannelHandler serverBootstrapAcceptor = channelPipeline.first();
-			System.out.println(serverBootstrapAcceptor.getClass().getName());
-			ChannelInitializer<SocketChannel> oldChildHandler = ReflectionUtils.getPrivateField(serverBootstrapAcceptor.getClass(), serverBootstrapAcceptor, ChannelInitializer.class, "childHandler");
-			ReflectionUtils.setFinalField(serverBootstrapAcceptor.getClass(), serverBootstrapAcceptor, "childHandler", new NettyChannelInitializer(oldChildHandler, minecraftServer.getClass().getPackage().getName()));
-		}
-	}
-	
-	public String getChannelFieldName(String version){
-		String name = "listeningChannels";
-		switch (version){
-			case "v1_12_R1":
-			case "v1_11_R1":
-			case "v1_10_R1":
-			case "v1_9_R2":
-			case "v1_9_R1":
-			case "v1_8_R2":
-			case "v1_8_R3":
-				name = "g";
-				break;
-			case "v1_14_R1":
-			case "v1_13_R1":
-			case "v1_13_R2":
-			case "v1_8_R1":
-				name = "f";
-				break;
-			case "v1_7_R4":
-				name = "e";
-				break;
-		}
-		return name;
-	}
+
+    private String channelFieldName;
+
+    public void onLoad() {
+        String version = super.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        channelFieldName = getChannelFieldName(version);
+        if (channelFieldName == null) {
+            getLogger().log(Level.SEVERE, "Unknown server version " + version + ", please see if there are any updates avaible");
+            return;
+        } else {
+            getLogger().info("Detected server version " + version);
+        }
+        try {
+            getLogger().info("Injecting NettyHandler...");
+            inject();
+            getLogger().info("Injection successful!");
+        } catch (Exception e) {
+            getLogger().info("Injection netty handler failed!");
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void inject() throws Exception {
+        Method serverGetHandle = Bukkit.getServer().getClass().getDeclaredMethod("getServer");
+        Object minecraftServer = serverGetHandle.invoke(Bukkit.getServer());
+
+        Method serverConnectionMethod = null;
+        for (Method method : minecraftServer.getClass().getSuperclass().getDeclaredMethods()) {
+            if (!method.getReturnType().getSimpleName().equals("ServerConnection")) {
+                continue;
+            }
+            serverConnectionMethod = method;
+            break;
+        }
+        Object serverConnection = serverConnectionMethod.invoke(minecraftServer);
+        List<ChannelFuture> channelFutureList = ReflectionUtils.getPrivateField(serverConnection.getClass(), serverConnection, List.class, channelFieldName);
+
+        for (ChannelFuture channelFuture : channelFutureList) {
+            ChannelPipeline channelPipeline = channelFuture.channel().pipeline();
+            ChannelHandler serverBootstrapAcceptor = channelPipeline.first();
+            System.out.println(serverBootstrapAcceptor.getClass().getName());
+            ChannelInitializer<SocketChannel> oldChildHandler = ReflectionUtils.getPrivateField(serverBootstrapAcceptor.getClass(), serverBootstrapAcceptor, ChannelInitializer.class, "childHandler");
+            ReflectionUtils.setPrivateField(serverBootstrapAcceptor.getClass(), serverBootstrapAcceptor, "childHandler", new NettyChannelInitializer(oldChildHandler, minecraftServer.getClass().getPackage().getName()));
+        }
+    }
+
+    public String getChannelFieldName(String version) {
+        switch (version) {
+            case "v1_16_R3":
+            case "v1_16_R2":
+            case "v1_16_R1":
+            case "v1_15_R1":
+                return "listeningChannels";
+            case "v1_12_R1":
+            case "v1_11_R1":
+            case "v1_10_R1":
+            case "v1_9_R2":
+            case "v1_9_R1":
+            case "v1_8_R2":
+            case "v1_8_R3":
+                return "g";
+            case "v1_17_R1":
+            case "v1_14_R1":
+            case "v1_13_R1":
+            case "v1_13_R2":
+            case "v1_8_R1":
+                return "f";
+            case "v1_7_R4":
+                return "e";
+        }
+        return null;
+    }
 }
--- src/main/java/nl/thijsalders/spigotproxy/netty/NettyChannelInitializer.java
+++ src/main/java/nl/thijsalders/spigotproxy/netty/NettyChannelInitializer.java
@@ -1,58 +1,66 @@
 package nl.thijsalders.spigotproxy.netty;
 
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.haproxy.HAProxyMessage;
+import io.netty.handler.codec.haproxy.HAProxyMessageDecoder;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
-import nl.thijsalders.spigotproxy.haproxy.HAProxyMessage;
-import nl.thijsalders.spigotproxy.haproxy.HAProxyMessageDecoder;
-
 public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
 
-	
-	private ChannelInitializer<SocketChannel> oldChildHandler;
-	private Method oldChildHandlerMethod;
-	private Field addr;
-	
-	public NettyChannelInitializer(ChannelInitializer<SocketChannel> oldChildHandler, String minecraftPackage) throws Exception {
-		this.oldChildHandler = oldChildHandler;
-		this.oldChildHandlerMethod = this.oldChildHandler.getClass().getDeclaredMethod("initChannel", Channel.class);
-		this.oldChildHandlerMethod.setAccessible(true);
 
-		Class<?> networkManager = Class.forName(minecraftPackage + ".NetworkManager");
-		try {
-			this.addr = networkManager.getField("socketAddress");
-		} catch (NoSuchFieldException e) {
-			this.addr = networkManager.getField("l");
-		}
-	}
-	
-	@Override
-	protected void initChannel(SocketChannel channel) throws Exception {
-		this.oldChildHandlerMethod.invoke(this.oldChildHandler, channel);
-		
-		channel.pipeline().addAfter("timeout", "haproxy-decoder", new HAProxyMessageDecoder());
-		channel.pipeline().addAfter("haproxy-decoder", "haproxy-handler", new ChannelInboundHandlerAdapter(){
-        	@Override
+    private final ChannelInitializer<SocketChannel> oldChildHandler;
+    private final Method oldChildHandlerMethod;
+    private Field addr;
+
+    public NettyChannelInitializer(ChannelInitializer<SocketChannel> oldChildHandler, String minecraftPackage) throws Exception {
+        this.oldChildHandler = oldChildHandler;
+        this.oldChildHandlerMethod = this.oldChildHandler.getClass().getDeclaredMethod("initChannel", Channel.class);
+        this.oldChildHandlerMethod.setAccessible(true);
+
+        Class<?> networkManager;
+        try {
+            networkManager = Class.forName("net.minecraft.network.NetworkManager");
+        } catch (ClassNotFoundException e) {
+            networkManager = Class.forName(minecraftPackage + ".NetworkManager");
+        }
+        try {
+            this.addr = networkManager.getField("socketAddress");
+        } catch (NoSuchFieldException e) {
+            this.addr = networkManager.getField("l");
+        }
+    }
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        this.oldChildHandlerMethod.invoke(this.oldChildHandler, channel);
+
+        channel.pipeline().addAfter("timeout", "haproxy-decoder", new HAProxyMessageDecoder());
+        channel.pipeline().addAfter("haproxy-decoder", "haproxy-handler", new ChannelInboundHandlerAdapter() {
+            @Override
             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                 if (msg instanceof HAProxyMessage) {
                     HAProxyMessage message = (HAProxyMessage) msg;
                     String realaddress = message.sourceAddress();
                     int realport = message.sourcePort();
-                    
+
                     SocketAddress socketaddr = new InetSocketAddress(realaddress, realport);
 
                     ChannelHandler handler = channel.pipeline().get("packet_handler");
-					addr.set(handler, socketaddr);
+                    addr.set(handler, socketaddr);
                 } else {
                     super.channelRead(ctx, msg);
                 }
             }
-		});
-	}
+        });
+    }
 
 }
